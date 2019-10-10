a15e8b1d1078af439346e2c1701762dce1b92a84
Olli Pertovaara <c7operto@kc408mac17.tamk.ad>
2019-10-10 17:10:36 +0300

    Initial Commit

---
+++ weather-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather-app
+//
+//  Created by Olli Pertovaara on 10/10/2019.
+//  Copyright © 2019 Olli Pertovaara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
