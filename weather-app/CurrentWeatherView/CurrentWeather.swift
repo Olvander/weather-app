18882c67e718854f2bc0182ea9a26774ce97c687
Olvander <olli.pertovaara@gmail.com>
2019-10-10 18:42:32 +0300

    Add files CurrentWeather.swift and CurrentWeather.xib

---
+++ weather-app/CurrentWeatherView/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  weather-app
+//
+//  Created by Olli Pertovaara on 10/10/2019.
+//  Copyright © 2019 Olli Pertovaara. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeather: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
