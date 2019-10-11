11b9979ca0efb56f6cd0bb82f20ed943d177fb81
Olvander <olli.pertovaara@gmail.com>
2019-10-11 16:44:52 +0300

    Update file CurrentWeatherView
    To show the current temperature in Tampere.

--- weather-app/New Group/CurrentWeatherView.swift
+++ weather-app/New Group/CurrentWeatherView.swift
@@ -20,4 +20,54 @@ class CurrentWeatherView: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        fetchUrl(url: "https://api.openweathermap.org/data/2.5/weather?q=Tampere,finland&units=metric&APPID=a999e5bd758a659bb04ec14a1df4cb0a")
+    }
+    
+    func fetchUrl(url : String) {
+        let config = URLSessionConfiguration.default
+        
+        let session = URLSession(configuration: config)
+        
+        let url : URL? = URL(string: url)
+        
+        let task = session.dataTask(with: url!, completionHandler: doneFetching);
+        
+        // Starts the task, spawns a new thread and calls the callback function
+        task.resume();
+    }
+    
+    func doneFetching(data: Data?, response: URLResponse?, error: Error?) {
+        let resstr = String(data: data!, encoding: String.Encoding.utf8)
+        
+        // Execute stuff in UI thread
+        DispatchQueue.main.async(execute: {() in
+            NSLog(resstr!)
+            self.setLabelTexts(data: data!)
+        })
+    }
+    
+    func setLabelTexts(data currentWeather: Data) {
+        
+        var jsonObj: NSDictionary?
+        
+        do {
+             jsonObj = try JSONSerialization.jsonObject(with: currentWeather, options: .mutableContainers) as? NSDictionary
+        } catch {
+            print("An error occurred serializing the current weather data")
+        }
+        
+        if let json = jsonObj {
+            
+            if let name = json["name"] as? String {
+                cityLabel!.text = name
+            }
+            if let main = json["main"] as? NSDictionary {
+                if let temperature = main["temp"] as? Double {
+                    temperatureLabel!.text = String(format: "%.1f", temperature)
+                }
+             }
+        }
+    }
 }
