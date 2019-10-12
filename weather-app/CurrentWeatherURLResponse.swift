c3f6b126c06b0d0ea283c2a3447294cc4a206419
Olvander <olli.pertovaara@gmail.com>
2019-10-12 17:46:00 +0300

    add a temp file to remove it later

---
+++ weather-app/CurrentWeatherURLResponse.swift
@@ -0,0 +1,15 @@
+//
+//  currentWeatherURLResponse.swift
+//  weather-app
+//
+//  Created by Olli Pertovaara on 12/10/2019.
+//  Copyright © 2019 Olli Pertovaara. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeatherURLResponse {
+    var currentWeather: String?
+    var weatherIcon: String?
+    var cityName: String?
+}
