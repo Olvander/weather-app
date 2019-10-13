b30c5a07338ff099c9e72f59b155f28a15c377bb
Olvander <olli.pertovaara@gmail.com>
2019-10-13 17:31:57 +0300

    Update CurrentWeatherView.swift and Main.storyboard.
    Improve Current Weather UI.

--- weather-app/New Group/CurrentWeatherView.swift
+++ weather-app/New Group/CurrentWeatherView.swift
@@ -7,23 +7,41 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class CurrentWeatherView: UIViewController {
     
+    @IBOutlet weak var cityView: UIView!
     @IBOutlet weak var cityLabel: UILabel!
     
     @IBOutlet weak var weatherIcon: UIImageView!
     
     
+    @IBOutlet weak var temperatureView: UIView!
     @IBOutlet weak var temperatureLabel: UILabel!
+    
+    var runAlready = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.title = "Current Weather"
+        self.view.backgroundColor = UIColor(red: 0.20, green: 0.78, blue: 0.95, alpha: 1.00)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.cityView!.layer.cornerRadius = 25
+        self.temperatureView!.layer.cornerRadius = 25
+        
+        self.cityView!.isHidden = true
+        self.cityLabel!.isHidden = true
+        self.temperatureView!.isHidden = true
+        self.temperatureLabel!.isHidden = true
+        self.weatherIcon!.isHidden = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
+
         fetchUrl(url: "https://api.openweathermap.org/data/2.5/weather?q=Tampere,finland&units=metric&APPID=a999e5bd758a659bb04ec14a1df4cb0a")
     }
     
@@ -66,6 +84,13 @@ class CurrentWeatherView: UIViewController {
             
             if let name = json["name"] as? String {
                 self.cityLabel!.text = name
+                self.cityLabel!.sizeToFit()
+                
+                var frame = CGRect(x: UIScreen.main.bounds.width / 2 - (self.cityLabel!.frame.width + 50) / 2, y: self.weatherIcon!.frame.origin.y - 50 - 8, width: self.cityLabel!.frame.width + 50.0, height: 50.0)
+                
+                self.cityView!.frame = frame
+                self.cityLabel!.center = self.cityView!.center
+                self.view.bringSubviewToFront(self.cityLabel!)
             }
             
             if let weather = json["weather"] as? Array<NSDictionary> {
@@ -75,7 +100,7 @@ class CurrentWeatherView: UIViewController {
                     
                     do {
                         let data = try Data(contentsOf: url!)
-                    self.weatherIcon!.image = UIImage(data: data)
+                        self.weatherIcon!.image = UIImage(data: data)
                     } catch {
                         print("Something went wrong while fetching the url \(url).")
                     }
@@ -85,8 +110,26 @@ class CurrentWeatherView: UIViewController {
             if let main = json["main"] as? NSDictionary {
                 if let temperature = main["temp"] as? Double {
                     self.temperatureLabel!.text = String(format: "%.1f", temperature)
+
+                    self.temperatureLabel!.sizeToFit()
+                    
+                    var frame = CGRect(x: UIScreen.main.bounds.width / 2 - (self.temperatureLabel!.frame.width + 50) / 2, y: self.weatherIcon!.frame.origin.y + self.weatherIcon!.frame.height + 8, width: self.temperatureLabel!.frame.width + 50.0, height: 50.0)
+                    
+                    self.temperatureView!.frame = frame
+                   
+                    self.temperatureLabel!.center = self.temperatureView!.center
                 }
             }
         }
+        self.cityView!.isHidden = false
+        self.cityLabel!.isHidden = false
+        self.temperatureView.isHidden = false
+        self.temperatureLabel!.isHidden = false
+        self.weatherIcon!.isHidden = false
     }
 }
+
+
+
+
+
