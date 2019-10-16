61963045b39d8b633ce64034c4a4b7a9b11151de
Olvander <olli.pertovaara@gmail.com>
2019-10-16 13:37:02 +0300

    Add CustomLocationCell again.

---
+++ weather-app/CustomLocationCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLocationCell.swift
+//  weather-app
+//
+//  Created by Olli Pertovaara on 16/10/2019.
+//  Copyright © 2019 Olli Pertovaara. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomLocationCell {
+    
+    func configureCityCell(location: Location) {
+        
+    }
+}
